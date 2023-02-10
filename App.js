56cf0187364ea563fbda5af103048a095c4e4aa7
DudiAM19 <dudiali79@gmail.com>
2023-02-10 10:02:58 +0700

    unit testing

---
+++ App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const changeDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+  return (
+    <View style={styles.newContainer(darkMode)}>
+      <TouchableOpacity onPress={changeDarkMode} style={styles.button(darkMode)}>
+        <Text style={styles.textButton(darkMode)}>Switch</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  newContainer: darkMode => ({
+    backgroundColor: darkMode ? '#282C35' : 'white',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }),
+  button: darkMode => ({
+    padding: 20,
+    backgroundColor: darkMode ? 'white' : '#282C35',
+    borderRadius: 20,
+  }),
+  textButton: darkMode => ({
+    color: darkMode ? '#282C35' : 'white',
+    fontWeight: 'bold'
+  }),
+});
+
+export default App;
